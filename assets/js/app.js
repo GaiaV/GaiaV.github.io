1d12fa18c984201df793725f40bcdacf71c7dad2
Gaia <gaia.vergari@student.bau.cat>
2022-03-10 21:08:04 +0100

    10-03 - After class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,88 +1,47 @@
-/*
-    This pen cleverly utilizes SVG filters to create a "Morphing Text" effect. Essentially, it layers 2 text elements on top of each other, and blurs them depending on which text element should be more visible. Once the blurring is applied, both texts are fed through a threshold filter together, which produces the "gooey" effect. Check the CSS - Comment the #container rule's filter out to see how the blurring works!
-*/
+var btt = $("#back_to_top");
 
-const elts = {
-    text1: document.getElementById("text1"),
-    text2: document.getElementById("text2"),
-};
+$(document).ready(function () {
 
-// The strings to morph between. You can change these to anything you want!
-const texts = ["a creative", "a graphic designer", "a web designer", "a photographer", "a ux/ui designer", "an illustrator", "...what else?"];
 
-// Controls the speed of morphing.
-const morphTime = 1.5;
-const cooldownTime = 0.25;
 
-let textIndex = texts.length - 1;
-let time = new Date();
-let morph = 0;
-let cooldown = cooldownTime;
+    btt.click(function () {
 
-elts.text1.textContent = texts[textIndex % texts.length];
-elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+        // torno ad inizio pagina con movenza fluida
 
-function doMorph() {
-    morph -= cooldown;
-    cooldown = 0;
+        $("html,body").animate({ scrollTop: 0 }, 500, function () { });
 
-    let fraction = morph / morphTime;
+    });
 
-    if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-    }
+    $(".js-hamb").on("click", function (event) {
 
-    setMorph(fraction);
-}
+        event.preventDefault();
 
-// A lot of the magic happens here, this is what applies the blur filter to the text.
-function setMorph(fraction) {
-    // fraction = Math.cos(fraction * Math.PI) / -2 + .5;
+        $("html").toggleClass("menu-open");
 
-    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+    });
 
-    fraction = 1 - fraction;
-    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+});
 
-    elts.text1.textContent = texts[textIndex % texts.length];
-    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-}
 
-function doCooldown() {
-    morph = 0;
 
-    elts.text2.style.filter = "";
-    elts.text2.style.opacity = "100%";
+$(window).scroll(function () {
 
-    elts.text1.style.filter = "";
-    elts.text1.style.opacity = "0%";
-}
+    // se lo scroll supera i 300 pixel dal margine superiore mostro il bottone (se nascosto)
 
-// Animation loop, which is called every frame.
-function animate() {
-    requestAnimationFrame(animate);
+    if ($(window).scrollTop() > 300) {
 
-    let newTime = new Date();
-    let shouldIncrementIndex = cooldown > 0;
-    let dt = (newTime - time) / 1000;
-    time = newTime;
+        if (btt.is(":hidden")) btt.fadeIn();
 
-    cooldown -= dt;
+        // in caso contrario lo nascondo (se visibile)
 
-    if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-            textIndex++;
-        }
-
-        doMorph();
     } else {
-        doCooldown();
+
+        if (btt.is(":visible")) btt.fadeOut(200);
+
     }
-}
 
-// Start the animation.
-animate();
-
+    var pixels = $(document).scrollTop();
+    var pageHeight = $(document).height() - $(window).height();
+    var progress = 100 * pixels / pageHeight;
+    $("div.progress").css("width", progress + "%");
+})
